7a55a20ecb2a3a1be44b04410e3dce1f24821417
Bigsk <i@ianxia.com>
2025-04-06 02:48:30 +0800

    Fixed white-screen bug on Safari

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,18 +22,24 @@ export default {
         <div class="mx-20 flex-col">
           <img
             src="/logo.svg"
-            class="h-80 mt-36 max-xl:mt-26 max-xl:h-60 max-sm:mt-16 max-sm:h-40"
+            class="h-60 mt-36 max-xl:mt-26 max-xl:h-60 max-sm:mt-16 max-sm:h-40"
           />
           <p
-            class="mt-16 mb-36 text-3xl max-sm:text-2xl text-gray-100 dark:text-slate-100 font-bold"
+            class="mt-16 mb-30 text-3xl max-sm:text-2xl text-gray-100 dark:text-slate-100 font-bold"
           >
             Forge Alchemy, Sculpt Epochs.
           </p>
           <div class="md:flex md:items-center mt-6">
-            <a href="https://github.com/csaslu" class="ml-5">
+            <button
+                @click="scrollIntoView"
+                class="hidden duration-300 ease-in-out bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded-lg"
+            >
+              Learn More
+            </button>
+            <a href="https://github.com/csaslu">
               <img
                 src="/Github.svg"
-                class="h-16 mt-4 max-sm:mt-2 max-sm:h-112 invert dark:filter dark:invert"
+                class="h-16 mt-4 max-sm:mt-20 max-sm:h-112 invert dark:filter dark:invert"
               />
             </a>
           </div>
